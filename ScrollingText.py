3fcb7b218df247cd49f889c1ba273e1684179043
generaldave <icepicktoeye@gmail.com>
2016-12-04 23:54:53 -0500

    Commented and Cleaned code

--- ScrollingText.py
+++ ScrollingText.py
@@ -1,35 +1,68 @@
+########################################################################
+#                                                                      #
+# David Fuller                                                         #
+#                                                                      #
+# ScrollingText Class for a Simple MP3 Player                          #
+#                                                                      #
+# Created on 2016-12-3                                                 #
+#                                                                      #
+########################################################################
+
+
+########################################################################
+#                                                                      #
+#                          IMPORT STATEMENTS                           #
+#                                                                      #
+########################################################################
+
 import pygame   # For GUI
 
-class ScrollingText(object):
-    def __init__(self, surface, text, fontSize, x, y, \
-                 areaCoord, fps, color, seconds):
-        self.counter = 1
-        pygame.font.init()
-        self.font = pygame.font.SysFont("Helvetica", fontSize)
-        
-        self.surface = surface
-        self.text = text
-        self.x = x
-        self.y = y
-        self.color = color
+#######################################################################
+#                                                                     #
+#                         SCROLLINGTEXT CLASS                         #
+#                                                                     #
+#######################################################################
 
+class ScrollingText(object):
+    def __init__(self, screen, text, fontSize, x, y, \
+                 areaCoord, fps, color, seconds):
+        self.screen = screen   # Screen
+        self.fps    = fps      # Frames per second
+        self.font   = pygame.font.SysFont("Helvetica", fontSize)
+        pygame.font.init()
+        
+        # Scrolling text information
+        self.text      = text
+        self.textColor = color
+        self.x         = x
+        self.y         = y
+        self.areaCoord = areaCoord
+        self.position  = 0
+
+        # To handle scrolling
+        self.counter = 1
         self.seconds = seconds
 
-        self.areaCoord = areaCoord
-        self.position = 0
-
-        self.fps = fps
-
-        self.scrollingText = self.font.render(self.text, True,
-                                              self.color)
-        self.surface.blit(self.scrollingText, (self.x, self.y))
         
+        # Draw scrolling text
+        self.drawText()
+
+    # Method draws scrolling text
+    def drawText(self):
+        self.scrollingText = self.font.render(self.text, True,
+                                              self.textColor)
+        self.screen.blit(self.scrollingText, (self.x, self.y))
+
+    # Method handles scrolling of text
     def update(self):
-        x = self.position
-        y = self.areaCoord[1]
-        width = self.areaCoord[2]
+        # Store coordinate varibales
+        x      = self.position
+        y      = self.areaCoord[1]
+        width  = self.areaCoord[2]
         height = self.areaCoord[3]
-        self.surface.blit(self.scrollingText, \
+
+        # Draw Text
+        self.screen.blit(self.scrollingText, \
                               (self.x, self.y), \
                               (x, y, width, height))
 
